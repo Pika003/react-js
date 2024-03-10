7c3d75f78ddec90a34d802092ac07d708c2e469e
Pika003 <satyajitmaity0123.com>
2024-03-10 20:32:37 +0530

    testing

---
+++ testing/src/App.jsx
@@ -0,0 +1,52 @@
+function App() {
+  return (
+    <>
+      <div className="bg-orange-800 h-screen flex justify-center items-center md:bg-sky-500 gap-3">
+        <div className="p-5 flex-col bg-green-600 h-52 w-48 rounded-md md:bg-orange-400 md:w-80 md:h-80">
+          <div className="flex justify-center items-center gap-3 md:flex-col text-white">
+            <img
+              src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className="rounded-full w-10 overflow-hidden md:rounded-lg md:w-32 md:drop-shadow-xl"
+            />
+            <h2 className=" font-semibold font-mono">Rahul Das</h2>
+          </div>
+          <p className="py-3 text-start font-medium md:">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+            nam!
+          </p>
+        </div>
+        <div className="p-5 flex-col bg-green-600 h-52 w-48 rounded-md md:bg-orange-400 md:w-80 md:h-80">
+          <div className="flex justify-center items-center gap-3 md:flex-col text-white">
+            <img
+              src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className="rounded-full w-10 overflow-hidden md:rounded-lg md:w-32 md:drop-shadow-xl"
+            />
+            <h2 className=" font-semibold font-mono">Rahul Das</h2>
+          </div>
+          <p className="py-3 text-start font-medium md:">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+            nam!
+          </p>
+        </div>
+        <div className="p-5 flex-col bg-green-600 h-52 w-48 rounded-md md:bg-orange-400 md:w-80 md:h-80">
+          <div className="flex justify-center items-center gap-3 md:flex-col text-white">
+            <img
+              src="https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              className="rounded-full w-10 overflow-hidden md:rounded-lg md:w-32 md:drop-shadow-xl"
+            />
+            <h2 className=" font-semibold font-mono">Rahul Das</h2>
+          </div>
+          <p className="py-3 text-start font-medium md:">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+            nam!
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
